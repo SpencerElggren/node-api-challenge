f978fd74933bb2750d0a80e5b5b53cf5f7dbd4e7
ElkBranch <elggren.spencer@gmail.com>
2020-02-17 16:09:11 -0700

    MVP

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require('express');
+const actionM = require('data/helpers/actionModel');
+const projM = require('data/helpers/projectModel');
 
-Sing along:
+const server = express();
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+function validateId(req, res, next) {
+    const id = req.params.id;
+    if (id) {
+        next();
+    } else {
+        res.status(404);
+    }
+}
 
-Go code!
-*/
+
+
+server.get('/api/projects/:id' , validateId, (req, res) => {
+    const id = req.params.id;
+    projM.get(id).then(r => res.status(200).json(r))
+});
+
+server.get('/api/actions/:id', validateId, (req,res) => {
+    const id = req.params.id;
+    actionM.get(id).then(r => res.status(200).json(r))
+});
+
+server.get('/api/projects/:id/actions', validateId, (req, res) => {
+    const id = req.params.id;
+    projM.getProjectActions(id).then(r => res.status(200).json(r))
+});
+
+server.post('/api/projects', (req, res) => {
+    const project = req.body;
+    projM.insert(project).then(r => console.log(r))
+});
+
+server.post('/api/actions', (req, res) => {
+    const action = req.body;
+    actionM.insert(action).then(r => console.log(r))
+});
+
+server.put('/api/projects/:id', validateId, (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    projM.update(id, changes).then(r => console.log(r))
+});
+
+server.put('/api/actions/:id', validateId, (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    actionM.update(id, changes).then(r => console.log(r))
+});
+
+server.delete('/api/projects/:id' , validateId, (req, res) => {
+    const id = req.params.id;
+    projM.remove(id).then(r => console.log(r))
+});
+
+server.delete('/api/actions/:id', validateId, (req, res) => {
+    const id = req.params.id;
+    actionM.remove(id).then( r => console.log(r))
+});
